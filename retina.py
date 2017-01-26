e1cbcb75976d1cf33fe49432f259d8af6b2a9694
Ravi S. Jonnal <rjonnal@gmail.com>
2017-01-25 22:39:58 -0800

    mosaic formation working pretty well

--- retina.py
+++ retina.py
@@ -12,37 +12,52 @@ ACTIVATION_THRESHOLD = 20.0
 class Cone:
 
     index = 0
-    slope = -10
+    slope = -20000
     
     def __init__(self,x,y):
-        self.x = x
-        self.y = y
+        self.x = int(x)
+        self.y = int(y)
         self.activation = np.inf
         self.index = Cone.index
         Cone.index = Cone.index + 1
-        
-    def step(self,retina,N=360):
 
-        theta = np.linspace(0,np.pi*2,N)
-        #xx = retina.xidx(self.x+np.cos(theta)*step_size)
-        #yy = retina.yidx(self.y+np.sin(theta)*step_size)
+        #self.x_steps = np.array([-1,0,1,-1,0,1,-1,0,1]).astype(np.int)
+        #self.y_steps = np.array([-1,-1,-1,0,0,0,1,1,1]).astype(np.int)
 
-        xx = np.clip(retina.xidx(self.x) + np.array([-1,0,1,-1,1,-1,0,1]),0,retina.N-1).astype(np.int)
-        yy = np.clip(retina.yidx(self.y) + np.array([-1,-1,-1,0,0,1,1,1]),0,retina.N-1).astype(np.int)
+        rad = 1
+        self.x_steps,self.y_steps = np.meshgrid(np.arange(-rad,rad+1),np.arange(-rad,rad+1))
+        self.x_steps = self.x_steps.ravel()
+        self.y_steps = self.y_steps.ravel()
         
-        field = retina.field[xx,yy]
+    def step(self,retina):
+
+        xx = self.x + self.x_steps
+        yy = self.y + self.y_steps
+
+        xx = xx%retina.N
+        yy = yy%retina.N
+
+        field = retina.field[yy,xx]
         #noise = np.mean(field)*np.random.randn(len(field))*.1
         winner = np.argmin(field)
+
+        #plt.figure()
+        #plt.imshow(retina.field,interpolation='none')
         
-        self.x = retina.xdeg(xx[winner])
-        self.y = retina.ydeg(yy[winner])
+        #print xx
+        #print yy
+        #print field
+        #print winner
+        self.x = xx[winner]
+        self.y = yy[winner]
+        
         self.activation = field[winner]
         
         
 
 class Retina:
 
-    def __init__(self,x1=-0.5,x2=0.5,y1=-0.5,y2=0.5,N=1024):
+    def __init__(self,x1=-0.5,x2=0.5,y1=-0.5,y2=0.5,N=1024,k=2):
         
         self.N = N
         self.x1 = min(x1,x2)
@@ -56,23 +71,35 @@ class Retina:
         self.xstep = float(self.dx)/float(N)
         self.ystep = float(self.dy)/float(N)
         
-        self.XX,self.YY = np.meshgrid(np.arange(self.x1,self.x2,self.xstep),np.arange(self.y1,self.y2,self.ystep))
+        #self.XX,self.YY = np.meshgrid(np.arange(self.x1,self.x2+self.xstep,self.xstep),np.arange(self.y1,self.y2+self.ystep,self.ystep))
+        self.XX,self.YY = np.meshgrid(np.linspace(self.x1,self.x2,self.N),np.linspace(self.y1,self.y2,self.N))
 
+        #for idx,x in enumerate(self.XX[0,:]):
+        #    print idx,x
+        #sys.exit()
+        
         self.cones = []
         self.age = 0
         self.clims = None
         self.field = np.zeros(self.XX.shape)
+        self.centers = np.zeros(self.XX.shape)
+        self.k = k
         
     def get_random_coordinates(self):
         return np.random.randint(self.N),np.random.randint(self.N)
 
 
     def xidx(self,x):
-        return np.clip(np.round((x-self.x1)/self.dx*self.N).astype(np.int),0,self.N-1)
-        
+        # original: get stuck at edges:
+        # np.clip(np.round((y-self.y1)/self.dy*self.N).astype(np.int),0,self.N-1)
+        # new idea: just return the right indices and have the caller sort it out
+        idx = (x-self.x1)/self.dx*self.N
+        return idx
+    
     def yidx(self,y):
-        return np.clip(np.round((y-self.y1)/self.dy*self.N).astype(np.int),0,self.N-1)
-
+        idx = (y-self.y1)/self.dy*self.N
+        return idx
+    
     def xdeg(self,x):
         return self.XX[0,x]
 
@@ -81,15 +108,13 @@ class Retina:
         
     def add(self,x=None,y=None):
         if x is None or y is None or xidx is None or yidx is None:
-            xidx,yidx = self.get_random_coordinates()
-            x = self.XX[0,xidx]
-            y = self.YY[yidx,0]
+            x,y = self.get_random_coordinates()
             
         print 'Adding cone at %0.1f,%0.1f'%(x,y)
         self.cones.append(Cone(x,y))
 
     def get_central_field(self):
-        return np.sqrt(self.XX**2+self.YY**2)*0
+        return np.sqrt(self.XX**2+self.YY**2)*5
 
     def get_cone_field(self):
         xx = self.XX - np.mean(self.XX)
@@ -100,8 +125,8 @@ class Retina:
     def compute_total_field(self):
         centers = np.zeros(self.XX.shape)
 
-        xidx = [self.xidx(c.x) for c in self.cones]
-        yidx = [self.yidx(c.y) for c in self.cones]
+        xidx = [c.x for c in self.cones]
+        yidx = [c.y for c in self.cones]
 
         for x,y in zip(xidx,yidx):
             centers[y,x] = centers[y,x] + 1
@@ -111,45 +136,32 @@ class Retina:
         cone_field = self.get_cone_field()
 
         # convolve cone centers with the single cone field to get the aggregate field
-        def conv(a,b,k=2):
-            sy,sx = a.shape
-            # fft both
-            af = np.fft.fft2(a,s=(sy*k,sx*k))
-            bf = np.fft.fft2(b,s=(sy*k,sx*k))
-
-            # block swap them and multiply
-            #afs = np.fft.fftshift(af)
-            #bfs = np.fft.fftshift(bf)
-            #abf = afs*bfs
+        def conv(a,b):
             
+            sy,sx = a.shape
+            # fft both after doubling size w/ zero-padding
+            # this prevents circular convolution
+            af = np.fft.fft2(a,s=(sy*2,sx*2))
+            bf = np.fft.fft2(b,s=(sy*2,sx*2))
+
             # multiply
             abf = af*bf
-            #abf = np.fft.ifftshift(abf)
             
             # inverse fft
             abfi = np.fft.ifft2(abf)
-            #abfi = np.fft.ifftshift(abfi)
+
+            # crop first (sy+1)//2-1 pixels because of zero-padding
+            y1 = (sy+1)//2-1
+            y2 = y1+sy
+            x1 = (sx+1)//2-1
+            x2 = x1+sx
+            abfi = abfi[y1:y2,x1:x2]
+            
             return np.abs(abfi)
 
-        field1 = conv(centers,cone_field)
-        #field1 = np.abs(np.fft.ifftshift(np.fft.ifft2((np.fft.fft2(centers)*np.fft.fft2(cone_field)))))
-        field2 = convolve2d(centers,cone_field,mode='same')
-
-        
-        plt.figure()
-        plt.subplot(2,2,1)
-        plt.imshow(centers,interpolation='none')
-        plt.colorbar()
-        plt.subplot(2,2,2)
-        plt.imshow(cone_field,interpolation='none')
-        plt.colorbar()
-        plt.subplot(2,2,3)
-        plt.imshow(field1,interpolation='none')
-        plt.colorbar()
-        plt.subplot(2,2,4)
-        plt.imshow(field2,interpolation='none')
-        plt.colorbar()
-        plt.show()
+        field = conv(centers,cone_field)
+        #field_sanity_check = convolve2d(centers,cone_field,mode='same')
+        #assert(np.allclose(field,field_sanity_check))
         
         # add the central potential
         field = field + self.get_central_field()
@@ -157,14 +169,14 @@ class Retina:
         self.field = field
 
     def step(self,do_plot=True):
-        self.compute_total_field()
         if do_plot:
             self.show()
         self.age = self.age + 1
-        for c in self.cones:
-            print c.index
+        for idx,c in enumerate(self.cones):
             c.step(self)
-        print 'Age: %d'%self.age
+            if idx%10==0:
+                self.compute_total_field()
+        #print 'Age: %d'%self.age
 
     def show(self):
         if self.clims is None:
@@ -174,14 +186,15 @@ class Retina:
         plt.imshow(self.centers,extent=[self.XX.min(),self.XX.max(),self.YY.min(),self.YY.max()],interpolation='none')
         plt.colorbar()
         plt.subplot(1,2,2)
-        plt.imshow(self.field,extent=[self.XX.min(),self.XX.max(),self.YY.min(),self.YY.max()],interpolation='none')
+        #plt.imshow(self.field,extent=[self.XX.min(),self.XX.max(),self.YY.min(),self.YY.max()],interpolation='none')
+        plt.imshow(self.field,interpolation='none')
         plt.colorbar()
         plt.autoscale(False)
         cx = [c.x for c in self.cones]
         cy = [c.y for c in self.cones]
         plt.plot(cx,cy,'ko')
-        for x,y in zip(cx,cy):
-            print 'Cone at %0.3f,%0.3f'%(x,y)
+        #for x,y in zip(cx,cy):
+            #print 'Cone at %0.3f,%0.3f'%(x,y)
         
         #plt.ylim((self.YY.max(),self.YY.min()))
         
@@ -199,29 +212,29 @@ class Retina:
 if __name__=='__main__':
 
 
-    mini = True
+    mini = False
     
     if mini:
-        r = Retina(x1=-.25,x2=.25,y1=-.25,y2=.25,N=32)
-        for k in range(3):
+        r = Retina(x1=-.25,x2=.25,y1=-.25,y2=.25,N=51)
+        for k in range(200):
             r.add()
 
         plt.figure()
-        for k in range(20):
+        for k in range(10):
             r.step()
-            plt.show()
-            plt.pause(5)
+            #plt.show()
+            plt.pause(.0001)
 
     else:
-        r = Retina(x1=-.25,x2=.25,y1=-.25,y2=.25,N=1000)
-        for k in range(4500):
+        r = Retina(x1=-.25,x2=.25,y1=-.25,y2=.25,N=255)
+        for k in range(450):
             r.add()
             #r.show(xx,yy)
             #plt.pause(.00001)
 
         mov = GIF('retina.gif')
         f = plt.figure()
-        for k in range(100):
+        for k in range(10):
             t0 = time()
             r.step()
             plt.pause(.001)
